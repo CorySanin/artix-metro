09a5ba8913c787d24a482f564de3f5b663df79ba
Cory Sanin <corysanin@outlook.com>
2023-05-11 03:56:36 -0500

    using json5 now suck it, Douglas Crockford

--- testingpkg-kde.sh
+++ testingpkg-kde.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-node index.js --job jobs/kde-01.json && \
+node index.js --job jobs/kde-01.json5 && \
 btimport spectacle && \
 node rm-systemd.js --pkgbuild ~/Documents/pkg/artixlinux/desktop/spectacle/trunk/PKGBUILD &&
 testingpkg -p spectacle && \
-node index.js --job jobs/kde-02.json && \
+node index.js --job jobs/kde-02.json5 && \
 btimport dolphin && \
 node rm-systemd.js --pkgbuild ~/Documents/pkg/artixlinux/desktop/dolphin/trunk/PKGBUILD && \
 testingpkg -p dolphin && \
-node index.js --job jobs/kde-03.json;
+node index.js --job jobs/kde-03.json5;
 curl --header "Content-Type: application/json" --request POST --data '{"packages": "batch done"}' 192.168.1.250:4444/artix
