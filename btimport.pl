10096a4352fe1fa0ef8c113ee9f341cce13bd33c
Cory Sanin <corysanin@outlook.com>
2023-05-11 03:57:12 -0500

    Only use artix-cmake if it's already there

--- btimport.pl
+++ btimport.pl
@@ -5,13 +5,17 @@ use warnings;
 
 undef $/;
 for (@ARGV) {
-    my @args = ("buildtree", "-ip", "$_");
-    exit 255 if system(@args) == 255;
     my @files = glob "~/Documents/pkg/artixlinux/*/$_/trunk/PKGBUILD";
     exit 1 if @files != 1;
     my $f = $files[0];
     open(FILE,$f);
     my $content = <FILE>;
+    close(FILE);
+    my @args = ("buildtree", "-ip", "$_");
+    exit 255 if system(@args) == 255;
+    exit 0 if $content !~ /artix-cmake\s/;
+    open(FILE,$f);
+    $content = <FILE>;
     close(FILE);
     $content =~ s/(cmake (.|\\\n)*-B)/artix-$1/g;
     open(FILE,">$f");
