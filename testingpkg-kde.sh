c32bef68f8bae3e1908d56976d9c814317f9e3ab
Cory Sanin <corysanin@outlook.com>
2025-11-18 13:43:08 -0500

    use jsonc extension for better IDE support

--- testingpkg-kde.sh
+++ testingpkg-kde.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -s -p "Enter your GPG password: " GPGPASS
 export GPGPASS
-artix-metro --job jobs/kde-add.json5 \
-&& artix-metro --job jobs/kde-move.json5
+artix-metro --job jobs/kde-add.jsonc \
+&& artix-metro --job jobs/kde-move.jsonc
 
 ./notify.sh
 GPGPASS=""
