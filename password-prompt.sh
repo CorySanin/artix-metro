da3bd838a6fb5208709fbbc25432c7c2096a2ef8
Cory Sanin <corysanin@outlook.com>
2026-01-11 16:22:24 -0500

    fix kde script

--- password-prompt.sh
+++ password-prompt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 read -s -p "Enter your GPG password: " GPGPASS
 export GPGPASS
+echo ""
--- testingpkg-kde.sh
+++ testingpkg-kde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./password-prompt.sh
-artix-metro --job jobs/kde-add.jsonc \
-&& artix-metro --job jobs/kde-move.jsonc
+source ./password-prompt.sh
+artix-metro --job jobs/kde-add.jsonc && \
+artix-metro --job jobs/kde-move.jsonc
 
 ./notify.sh
 GPGPASS=""
